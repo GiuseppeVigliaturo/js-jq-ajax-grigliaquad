628355ac7b3d836cf74131ef0e0a28d4c35e0453
giuviglia94 <vigliaturo94@gmail.com>
2019-11-18 19:38:57 +0100

    esercizio base

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+// DESCRIZIONE:
+//  Griglia 6x6, ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9.
+// Se è <= 5 il quadrato diventa giallo,
+// se è > di 5 il quadrato diventa verde.
+// Il numero ottenuto appare al centro del quadrato
+
+
+
+
+
+$(document).ready(function() {
+
+$(".grid-item").click(function(){
+
+//salvo una variabile esterna alla chiamata ajax altrimenti non vedrebbe il this
+  var clicked= $(this);
+
+    $.ajax({
+      url : "https://ﬂynn.boolean.careers/exercises/api/random/int",
+      method : "GET",
+      success : function (data) {
+        if (data.response <= 5 ) {
+          clicked.css("background", "yellow");
+        }
+        else {
+          clicked.css("background", "green");
+        }
+            console.log(data);
+         },
+      error : function (errore) {
+           alert("E' avvenuto un errore. "+errore);
+         }
+       });
+
+
+});
+
+
+  });
